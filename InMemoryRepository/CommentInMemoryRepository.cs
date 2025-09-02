bd05ca7d9bc51748e199b6e6716707c22af480eb
victor <victorgrud@gmail.com>
2025-09-02 11:26:02 +0200

    initial commit

---
+++ InMemoryRepository/CommentInMemoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepository;
+
+public class CommentInMemoryRepository : ICommentRepository
+    
+{
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ InMemoryRepository/UserInMemoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepository;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    public Task<User> AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        throw new NotImplementedException();
+    }
+}
